b3e60016cc2051b5fad41d6aaf8db59209afc0db
sascha.iseli@gmx.ch <sascha.iseli@gmx.ch>
2011-12-25 15:10:44 +0000

    auch tcx files einlesen

---
+++ ch.opentrainingcenter.importer.tcx/src/ch/opentrainingcenter/importer/tcx/Activator.java
@@ -0,0 +1,36 @@
+package ch.opentrainingcenter.importer.tcx;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    public static final String BUNDLE_ID = "ch.opentrainingcenter.importer.tcx"; //$NON-NLS-1$
+
+    private static BundleContext context;
+
+    static BundleContext getContext() {
+        return context;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(final BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(final BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
+
+}
--- ch.opentrainingcenter.importer.tcx/src/ch/opentrainingcenter/importer/tcx/ConvertTcx.java
+++ ch.opentrainingcenter.importer.tcx/src/ch/opentrainingcenter/importer/tcx/ConvertTcx.java
@@ -1,4 +1,4 @@
-package ch.opentrainingcenter.importer.fitnesslog.converter;
+package ch.opentrainingcenter.importer.tcx;
 
 import java.io.File;
 import java.io.IOException;
@@ -8,6 +8,7 @@ import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.validation.Schema;
@@ -23,17 +24,14 @@ import org.xml.sax.SAXException;
 import ch.iseli.sportanalyzer.importer.IConvert2Tcx;
 import ch.iseli.sportanalyzer.tcx.ActivityT;
 import ch.iseli.sportanalyzer.tcx.TrainingCenterDatabaseT;
-import ch.opentrainingcenter.importer.fitnesslog.Activator;
-import ch.opentrainingcenter.importer.fitnesslog.model.FitnessWorkbook;
 
-public class ConvertFitnesslog2Tcx implements IConvert2Tcx {
-
-    private static final String RESOURCES_FITNESSLOGBOOK_XSD = "resources/fitnesslogbook.xsd";//$NON-NLS-1$
-    private static final Logger logger = Logger.getLogger(ConvertFitnesslog2Tcx.class);
+public class ConvertTcx implements IConvert2Tcx {
+    private static final String RESOURCES_FITNESSLOGBOOK_XSD = "resources/tcx.xsd";//$NON-NLS-1$
+    private static final Logger logger = Logger.getLogger(ConvertTcx.class);
     private final Bundle bundle;
     private final String locationOfScript;
 
-    public ConvertFitnesslog2Tcx() {
+    public ConvertTcx() {
         logger.info("ConvertFitnesslog2Tcx erfolgreich instanziert....");//$NON-NLS-1$
         bundle = Platform.getBundle(Activator.BUNDLE_ID);
         final Path path = new Path(RESOURCES_FITNESSLOGBOOK_XSD);
@@ -49,30 +47,22 @@ public class ConvertFitnesslog2Tcx implements IConvert2Tcx {
         locationOfScript = f.getAbsolutePath();
     }
 
-    public ConvertFitnesslog2Tcx(final boolean withJunit) {
-        bundle = null;
-        if (withJunit) {
-            logger.info("working with the test constructor!!!"); //$NON-NLS-1$
-        }
-        final File xsd = new File(RESOURCES_FITNESSLOGBOOK_XSD);
-        locationOfScript = xsd.getAbsolutePath();
-    }
-
     @Override
     public TrainingCenterDatabaseT convert(final File file) throws Exception {
-        final FitnessWorkbook workbook = unmarshall(file);
-        final ConvertWorkbook2Tcx convertWorkbook2Tcx = new ConvertWorkbook2Tcx();
-        return convertWorkbook2Tcx.convert(workbook);
+        final TrainingCenterDatabaseT database = unmarshall(file);
+        return database;
     }
 
-    protected FitnessWorkbook unmarshall(final File file) throws JAXBException, SAXException {
+    @SuppressWarnings("unchecked")
+    public TrainingCenterDatabaseT unmarshall(final File file) throws JAXBException, SAXException {
         final Unmarshaller unmarshaller = createUnmarshaller();
-        final FitnessWorkbook unmarshal = (FitnessWorkbook) unmarshaller.unmarshal(file);
-        return unmarshal;
+
+        final JAXBElement<TrainingCenterDatabaseT> jaxb = (JAXBElement<TrainingCenterDatabaseT>) unmarshaller.unmarshal(file);
+        return jaxb.getValue();
     }
 
     private Unmarshaller createUnmarshaller() throws JAXBException, SAXException {
-        final JAXBContext jc = JAXBContext.newInstance(FitnessWorkbook.class);
+        final JAXBContext jc = JAXBContext.newInstance(TrainingCenterDatabaseT.class);
         final Unmarshaller unmarshaller = jc.createUnmarshaller();
 
         final SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
@@ -89,7 +79,7 @@ public class ConvertFitnesslog2Tcx implements IConvert2Tcx {
 
     @Override
     public String getFilePrefix() {
-        return "fitlog"; //$NON-NLS-1$
+        return "tcx"; //$NON-NLS-1$
     }
 
 }
