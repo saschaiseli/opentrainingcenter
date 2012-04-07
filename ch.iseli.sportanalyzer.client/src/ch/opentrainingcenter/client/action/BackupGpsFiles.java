de31893bb2b79eb4f434668f121eb4a48631fc85
sascha.iseli@gmx.ch <sascha.iseli@gmx.ch>
2012-04-07 10:03:47 +0000

    backuptozip

--- ch.iseli.sportanalyzer.client/src/ch/opentrainingcenter/client/action/BackupGpsFiles.java
+++ ch.iseli.sportanalyzer.client/src/ch/opentrainingcenter/client/action/BackupGpsFiles.java
@@ -1,9 +1,16 @@
 package ch.opentrainingcenter.client.action;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.CoreException;
@@ -23,7 +30,6 @@ import ch.opentrainingcenter.client.Activator;
 import ch.opentrainingcenter.client.Application;
 import ch.opentrainingcenter.client.Messages;
 import ch.opentrainingcenter.client.PreferenceConstants;
-import ch.opentrainingcenter.client.helper.FileCopy;
 import ch.opentrainingcenter.importer.ConvertHandler;
 import ch.opentrainingcenter.importer.IConvert2Tcx;
 
@@ -81,18 +87,52 @@ public class BackupGpsFiles extends Action implements ISelectionListener, IWorkb
             @Override
             protected IStatus run(final IProgressMonitor monitor) {
                 monitor.beginTask(Messages.BackupGpsFiles_1, fileToCopy.length);
+
+                final Date date = new Date();
+                final SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd"); //$NON-NLS-1$
+                final String zipFileName = df.format(date) + ".zip"; //$NON-NLS-1$
+                final File zipFile = new File(destFolder, zipFileName);
+
+                ZipOutputStream out = null;
+                try {
+                    out = new ZipOutputStream(new FileOutputStream(zipFile));
+                } catch (final FileNotFoundException e) {
+                    logger.error(e.getMessage());
+                }
+                final byte[] buf = new byte[1024];
                 for (final String fileName : fileToCopy) {
-                    logger.debug("File: " + fileName + " kopieren..."); //$NON-NLS-1$ //$NON-NLS-2$
                     final File file = new File(source + File.separator + fileName);
                     try {
-                        final File fileDestination = new File(destination, file.getName());
-                        FileCopy.copyFile(file, fileDestination);
-                        monitor.worked(1);
+                        addToZip(out, buf, file);
+                    } catch (final FileNotFoundException e) {
+                        logger.error("Fehler beim Zippen: Konnte file nicht finden: " + e.getMessage()); //$NON-NLS-1$
                     } catch (final IOException e) {
-                        logger.error("Konnte File nicht kopieren: " + e.getMessage()); //$NON-NLS-1$
+                        logger.error("IOException beim Zippen: " + e.getMessage()); //$NON-NLS-1$
                     }
+                    monitor.worked(1);
+                }
+                try {
+                    out.close();
+                } catch (final IOException e) {
+                    logger.error(e.getMessage());
                 }
                 return Status.OK_STATUS;
+            }
+
+            private void addToZip(final ZipOutputStream out, final byte[] buf, final File file) throws FileNotFoundException, IOException {
+                final FileInputStream in = new FileInputStream(file);
+
+                // Add ZIP entry to output stream.
+                out.putNextEntry(new ZipEntry(file.getName()));
+
+                // Transfer bytes from the file to the ZIP file
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                // Complete the entry
+                out.closeEntry();
+                in.close();
             }
         };
         job.schedule();
