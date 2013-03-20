1c3e03039a6fe137135c507f071cf005ae1b0095
sascha.iseli@gmx.ch <sascha.iseli@gmx.ch>
2013-03-20 06:30:28 +0000

    Initial import.

---
+++ ch.opentrainingcenter.route.tests/src/ch/opentrainingcenter/route/impl/CompareRouteTest.java
@@ -0,0 +1,20 @@
+package ch.opentrainingcenter.route.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CompareRouteTest {
+    private CompareRoute route;
+
+    @Before
+    public void setUp() {
+        route = new CompareRoute();
+    }
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
