d8f2ff300e9b1273067f65625d8b2dfdee8ab614
sascha.iseli@gmx.ch <sascha.iseli@gmx.ch>
2013-03-20 06:28:40 +0000

    Initial import.

---
+++ ch.opentrainingcenter.route/src/ch/opentrainingcenter/route/ICompareRoute.java
@@ -0,0 +1,7 @@
+package ch.opentrainingcenter.route;
+
+import java.awt.Shape;
+
+public interface ICompareRoute {
+    boolean compareRoute(Shape reference, Shape object);
+}
---
+++ ch.opentrainingcenter.route/src/ch/opentrainingcenter/route/impl/CompareRoute.java
@@ -0,0 +1,14 @@
+package ch.opentrainingcenter.route.impl;
+
+import java.awt.Shape;
+
+import ch.opentrainingcenter.route.ICompareRoute;
+
+public class CompareRoute implements ICompareRoute {
+
+    @Override
+    public boolean compareRoute(final Shape reference, final Shape object) {
+        return false;
+    }
+
+}
