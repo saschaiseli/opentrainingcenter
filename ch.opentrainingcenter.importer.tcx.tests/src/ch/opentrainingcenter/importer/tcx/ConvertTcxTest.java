bd0e2dc0ad741641553cdb846e84e91978ecbd58
sascha iseli <sascha.iseli@gmx.ch>
2013-04-07 17:09:54 +0200

    TCX Converter erstellt

--- ch.opentrainingcenter.importer.tcx.tests/src/ch/opentrainingcenter/importer/tcx/ConvertTcxTest.java
+++ ch.opentrainingcenter.importer.tcx.tests/src/ch/opentrainingcenter/importer/tcx/ConvertTcxTest.java
@@ -1,40 +1,115 @@
 package ch.opentrainingcenter.importer.tcx;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import ch.opentrainingcenter.transfer.IStreckenPunkt;
+import ch.opentrainingcenter.transfer.ITrackPointProperty;
 import ch.opentrainingcenter.transfer.ITraining;
 
 @SuppressWarnings("nls")
 public class ConvertTcxTest {
+    private static final double DELTA = 0.00000001;
     private ConvertTcx converter;
-    private File file;
+    private File simple, simple_one_lap, simple_two_lap, simple_out_of_memory;
 
     @Before
     public void setUp() {
         converter = new ConvertTcx();
-        file = new File("resources", "simple.tcx");
+        simple = new File("resources", "simple.tcx");
+        simple_one_lap = new File("resources", "simple_one_lap.tcx");
+        simple_two_lap = new File("resources", "simple_two_lap.tcx");
+        simple_out_of_memory = new File("resources", "simple_out_of_memory.tcx");
+    }
+
+    @Test
+    public void testName() {
+        final String name = converter.getName();
+        assertEquals("TCX (mit ANT+ aus der Uhr exportiert)", name);
+    }
+
+    @Test
+    public void testPrefix() {
+        final String name = converter.getFilePrefix();
+        assertEquals("tcx", name);
     }
 
     @Test
     public void testTrainingCount() throws Exception {
-        final ITraining trainings = converter.convert(file);
+        final ITraining trainings = converter.convert(simple);
         assertNotNull("Ein Traininng", trainings);
     }
 
     @Test
     public void testTrainingMainProperties() throws Exception {
-        final ITraining training = converter.convert(file);
-        assertEquals("Testen des Startdatums: ", 22, training.getDatum());
-        assertEquals("Dauer des Laufes", 1000, training.getDauer(), 0.001);
-        assertEquals("Länge des Laufes", 2000, training.getLaengeInMeter(), 0.001);
-        assertEquals("MaxSpeed des Laufes", 4, training.getMaxSpeed(), 0.001);
-        assertEquals("MaxHeartBeat des Laufes", 150, training.getMaxHeartBeat(), 0.001);
-        assertEquals("AverageHeartBeat des Laufes", 170, training.getAverageHeartBeat(), 0.001);
+        final ITraining training = converter.convert(simple);
+        assertEquals("Testen des Startdatums: ", 1364897192000L, training.getDatum());
+        assertEquals("Dauer des Laufes", 810.61, training.getDauer(), DELTA);
+        assertEquals("Länge des Laufes", 2443.549074, training.getLaengeInMeter(), DELTA);
+        assertEquals("MaxSpeed des Laufes", 5.07697296, training.getMaxSpeed(), DELTA);
+        assertEquals("MaxHeartBeat des Laufes", 177, training.getMaxHeartBeat());
+        assertEquals("AverageHeartBeat des Laufes", 160, training.getAverageHeartBeat());
+    }
+
+    @Test
+    public void testTrainingTrackPoints() throws Exception {
+        final ITraining training = converter.convert(simple_one_lap);
+        final List<ITrackPointProperty> points = training.getTrackPoints();
+        assertNotNull(points);
+        assertFalse("Liste der punkte darf nicht leer sein.", points.isEmpty());
+        assertEquals(3, points.size());
+
+        assertPoint(points.get(0), 543, 0.576151550, 85, 1, 1364897192000L, 7.43025357, 46.94510135);
+        assertPoint(points.get(1), 544, 1.55577779, 86, 1, 1364897193000L, 7.43026547, 46.94510487);
+        assertPoint(points.get(2), 545, 2.89205694, 87, 1, 1364897194000L, 7.43028106, 46.94511082);
+    }
+
+    @Test
+    public void testTrainingTrackPointsTwoLaps() throws Exception {
+        final ITraining training = converter.convert(simple_two_lap);
+        final List<ITrackPointProperty> points = training.getTrackPoints();
+        assertNotNull(points);
+        assertFalse("Liste der punkte darf nicht leer sein.", points.isEmpty());
+        assertEquals(6, points.size());
+
+        assertPoint(points.get(0), 543, 0.576151550, 85, 1, 1364897192000L, 7.43025357, 46.94510135);
+        assertPoint(points.get(1), 544, 1.55577779, 86, 1, 1364897193000L, 7.43026547, 46.94510487);
+        assertPoint(points.get(2), 545, 2.89205694, 87, 1, 1364897194000L, 7.43028106, 46.94511082);
+
+        assertPoint(points.get(3), 543, 0.576151550, 85, 2, 1364897192000L, 7.43025357, 46.94510135);
+        assertPoint(points.get(4), 544, 1.55577779, 86, 2, 1364897193000L, 7.43026547, 46.94510487);
+        assertPoint(points.get(5), 545, 2.89205694, 87, 2, 1364897194000L, 7.43028106, 46.94511082);
+    }
+
+    @Test
+    public void testWatchOutOfMemory() throws Exception {
+        final ITraining training = converter.convert(simple_out_of_memory);
+        assertEquals("Testen des Startdatums: ", 1356702915000L, training.getDatum());
+        assertEquals("Dauer des Laufes", 4274.57, training.getDauer(), DELTA);
+        assertEquals("Länge des Laufes", 12305.6787, training.getLaengeInMeter(), DELTA);
+        assertEquals("MaxSpeed des Laufes", 4.48573303, training.getMaxSpeed(), DELTA);
+        assertEquals("MaxHeartBeat des Laufes", 184, training.getMaxHeartBeat());
+        assertEquals("AverageHeartBeat des Laufes", 150, training.getAverageHeartBeat());
+        assertEquals("Da keine GSP Daten da sind, muss etwas in die Notes geschrieben werden", ConvertTcx.NO_DATA, training.getNote());
+    }
+
+    private void assertPoint(final ITrackPointProperty point, final int hoehe, final double distance, final int herz, final int runde, final long zeit,
+            final double longitude, final double latitude) {
+        assertEquals("Höhe: ", hoehe, point.getAltitude());
+        assertEquals("Distanz: ", distance, point.getDistance(), DELTA);
+        assertEquals("Herz: ", herz, point.getHeartBeat());
+        assertEquals("Runde: ", runde, point.getLap());
+        assertEquals("Zeit: ", zeit, point.getZeit());
+        final IStreckenPunkt geo = point.getStreckenPunkt();
+        assertEquals("Longitude: ", longitude, geo.getLongitude(), DELTA);
+        assertEquals("Latitude: ", latitude, geo.getLatitude(), DELTA);
+        assertEquals("Laenge muss gleich wie andere sein", point.getDistance(), geo.getDistance(), DELTA);
     }
 }
--- ch.opentrainingcenter.importer.tcx/src/ch/opentrainingcenter/importer/tcx/ConvertTcx.java
+++ ch.opentrainingcenter.importer.tcx/src/ch/opentrainingcenter/importer/tcx/ConvertTcx.java
@@ -3,7 +3,9 @@ package ch.opentrainingcenter.importer.tcx;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import javax.xml.XMLConstants;
@@ -11,6 +13,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
@@ -22,14 +25,23 @@ import org.osgi.framework.Bundle;
 import org.xml.sax.SAXException;
 
 import ch.opentrainingcenter.core.importer.IConvert2Tcx;
+import ch.opentrainingcenter.tcx.ActivityLapT;
 import ch.opentrainingcenter.tcx.ActivityT;
+import ch.opentrainingcenter.tcx.HeartRateInBeatsPerMinuteT;
+import ch.opentrainingcenter.tcx.PositionT;
+import ch.opentrainingcenter.tcx.TrackT;
+import ch.opentrainingcenter.tcx.TrackpointT;
 import ch.opentrainingcenter.tcx.TrainingCenterDatabaseT;
 import ch.opentrainingcenter.transfer.ActivityExtension;
+import ch.opentrainingcenter.transfer.CommonTransferFactory;
+import ch.opentrainingcenter.transfer.IStreckenPunkt;
+import ch.opentrainingcenter.transfer.ITrackPointProperty;
 import ch.opentrainingcenter.transfer.ITraining;
 
 public class ConvertTcx implements IConvert2Tcx {
-    private static final Logger logger = Logger.getLogger(ConvertTcx.class);
+    private static final Logger LOGGER = Logger.getLogger(ConvertTcx.class);
     private static final String RESOURCES_TCX_XSD = "resources/tcx.xsd";//$NON-NLS-1$
+    protected static final String NO_DATA = "NO GPS DATA"; //$NON-NLS-1$
 
     private final String locationOfScript;
 
@@ -37,7 +49,7 @@ public class ConvertTcx implements IConvert2Tcx {
      * Contructor for Eclipse Extension
      */
     public ConvertTcx() {
-        logger.info("ConvertTcx erfolgreich instanziert....");//$NON-NLS-1$
+        LOGGER.info("ConvertTcx erfolgreich instanziert....");//$NON-NLS-1$
         final Bundle bundle = Platform.getBundle(Activator.BUNDLE_ID);
         final Path path = new Path(RESOURCES_TCX_XSD);
         final URL url = FileLocator.find(bundle, path, Collections.EMPTY_MAP);
@@ -45,19 +57,12 @@ public class ConvertTcx implements IConvert2Tcx {
         try {
             fileUrl = FileLocator.toFileURL(url);
         } catch (final IOException e) {
-            logger.error("Fehler beim Instanzieren von ConvertTcx: " + e.getMessage());//$NON-NLS-1$
+            LOGGER.error("Fehler beim Instanzieren von ConvertTcx: " + e.getMessage());//$NON-NLS-1$
             throw new RuntimeException(e);
         }
         final File f = new File(fileUrl.getPath());
         locationOfScript = f.getAbsolutePath();
-        logger.info("ConvertTcx erfolgreich instanziert....fertig");//$NON-NLS-1$
-    }
-
-    /**
-     * Constructor für Tests
-     */
-    protected ConvertTcx(final String locationOfXsd) {
-        locationOfScript = locationOfXsd;
+        LOGGER.info("ConvertTcx erfolgreich instanziert....fertig");//$NON-NLS-1$
     }
 
     @SuppressWarnings("unchecked")
@@ -84,18 +89,86 @@ public class ConvertTcx implements IConvert2Tcx {
         final TrainingCenterDatabaseT completeFile = unmarshall(file);
         final List<ActivityT> activities = completeFile.getActivities().getActivity();
         final ActivityT activity = activities.get(0);
-        final ActivityExtension ext = new ActivityExtension();
-        final long dateOfStart = activity.getId().toGregorianCalendar().getTimeInMillis();
-        // final double timeInSeconds = activity.;
-        final double distance;
-        final int avgHeartRate;
-        final int maxHeartBeat;
-
-        final ITraining training = null;// CommonTransferFactory.createTraining(dateOfStart,
-                                        // timeInSeconds, distance,
-                                        // avgHeartRate, maxHeartBeat,
-                                        // maximumSpeed, ext);
+        final ITraining training = create(activity);
         return training;
+    }
+
+    private ITraining create(final ActivityT activity) {
+        // datum
+        final XMLGregorianCalendar date = activity.getId();
+        final Date dateOfStart = date.toGregorianCalendar().getTime();
+        final List<ActivityLapT> laps = activity.getLap();
+        final List<ITrackPointProperty> trackPoints = new ArrayList<ITrackPointProperty>();
+        double distance = 0.0;
+        double timeInSeconds = 0.0;
+        int averageHeartRateBpm = 0;
+        int maxHeartBeat = 0;
+        int lapWithCardio = 0;
+        double maximumSpeed = 0;
+        int lapCount = 1;
+        for (final ActivityLapT lap : laps) {
+            final List<ITrackPointProperty> trackPointsOfLap = getTrackPointsOfLap(lap, lapCount);
+            trackPoints.addAll(trackPointsOfLap);
+            lapCount++;
+            distance += lap.getDistanceMeters();
+            if (lap.getMaximumSpeed() != null && maximumSpeed < lap.getMaximumSpeed()) {
+                maximumSpeed = lap.getMaximumSpeed();
+            }
+            timeInSeconds += lap.getTotalTimeSeconds();
+            if (!hasCardio(lap)) {
+                continue;
+            }
+            lapWithCardio++;
+            averageHeartRateBpm += lap.getAverageHeartRateBpm() != null ? lap.getAverageHeartRateBpm().getValue() : 0;
+            if (maxHeartBeat < lap.getMaximumHeartRateBpm().getValue()) {
+                maxHeartBeat = lap.getMaximumHeartRateBpm().getValue();
+            }
+            LOGGER.debug("lap: " + lap.getIntensity() + " distance: " + distance); //$NON-NLS-1$//$NON-NLS-2$
+        }
+        Integer avgHeartRate = null;
+        if (lapWithCardio > 0) {
+            avgHeartRate = averageHeartRateBpm / lapWithCardio;
+        } else {
+            avgHeartRate = 0;
+        }
+        final ActivityExtension activityExtension = new ActivityExtension();
+
+        final ITraining training = CommonTransferFactory.createTraining(dateOfStart.getTime(), timeInSeconds, distance, avgHeartRate, maxHeartBeat,
+                maximumSpeed, activityExtension);
+        training.setTrackPoints(trackPoints);
+        if (trackPoints.isEmpty()) {
+            training.setNote(NO_DATA);
+        }
+        return training;
+    }
+
+    private List<ITrackPointProperty> getTrackPointsOfLap(final ActivityLapT lap, final int lapCount) {
+        final List<ITrackPointProperty> property = new ArrayList<ITrackPointProperty>();
+        final List<TrackT> tracks = lap.getTrack();
+        for (final TrackT track : tracks) {
+            final List<TrackpointT> trackpoints = track.getTrackpoint();
+            for (final TrackpointT trackpoint : trackpoints) {
+                final int altitude = trackpoint.getAltitudeMeters().intValue();
+                final double distance = trackpoint.getDistanceMeters();
+                final HeartRateInBeatsPerMinuteT heartRateBpm = trackpoint.getHeartRateBpm();
+                int heartbeat = 0;
+                if (heartRateBpm != null) {
+                    heartbeat = heartRateBpm.getValue();
+                }
+                final long time = trackpoint.getTime().toGregorianCalendar().getTime().getTime();
+                final PositionT position = trackpoint.getPosition();
+                IStreckenPunkt geoPunkt = null;
+                final double longitude = position.getLongitudeDegrees();
+                final double latitude = position.getLatitudeDegrees();
+                geoPunkt = CommonTransferFactory.createStreckenPunkt(distance, longitude, latitude);
+                property.add(CommonTransferFactory.createTrackPointProperty(distance, heartbeat, altitude, time, lapCount, geoPunkt));
+            }
+        }
+        return property;
+    }
+
+    private static boolean hasCardio(final ActivityLapT lap) {
+        return lap.getMaximumHeartRateBpm() != null;
     }
 
     @Override
