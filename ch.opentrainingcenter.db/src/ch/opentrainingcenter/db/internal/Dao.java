361f41ce952a5e2f333ee128f6bb05e1197afc00
sascha iseli <sascha.iseli@gmx.ch>
2013-05-31 11:48:37 +0200

    move session

--- ch.opentrainingcenter.db/src/ch/opentrainingcenter/db/internal/Dao.java
+++ ch.opentrainingcenter.db/src/ch/opentrainingcenter/db/internal/Dao.java
@@ -21,6 +21,26 @@ public class Dao implements IDao {
         Assertions.notNull(config, "Datenbankkonfiguration darf nicht null sein");
         this.usage = usage;
         this.config = config;
+        final org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+        configuration.setProperties(config.getProperties());
+        configuration.setProperty("current_session_context_class", "thread");
+        configuration.setProperty("cache.provider_class", "org.hibernate.cache.NoCacheProvider");
+        configuration.setProperty("show_sql", String.valueOf(usage.isShowSql()));
+        configuration.setProperty("format_sql", String.valueOf(usage.isFormatSql()));
+        try {
+            configuration.addResource("Athlete.hbm.xml");//$NON-NLS-1$
+            configuration.addResource("Health.hbm.xml"); //$NON-NLS-1$
+            configuration.addResource("Weather.hbm.xml"); //$NON-NLS-1$
+            configuration.addResource("Training.hbm.xml"); //$NON-NLS-1$
+            configuration.addResource("Trainingtype.hbm.xml"); //$NON-NLS-1$
+            configuration.addResource("Tracktrainingproperty.hbm.xml"); //$NON-NLS-1$
+            configuration.addResource("Streckenpunkte.hbm.xml"); //$NON-NLS-1$
+            configuration.addResource("Planungwoche.hbm.xml"); //$NON-NLS-1$
+            configuration.addResource("Route.hbm.xml"); //$NON-NLS-1$
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (final MappingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -41,38 +61,7 @@ public class Dao implements IDao {
     @Override
     public Session getSession() {
         if (session == null) {
-            final org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
-            configuration.setProperties(config.getProperties());
-            // switch (usage) {
-            // case PRODUCTION:
-            //                sessionFactory = configuration.configure("hibernate.cfg.xml").buildSessionFactory(); //$NON-NLS-1$
-            // break;
-            // case DEVELOPING:
-            //                sessionFactory = configuration.configure("hibernate_dev.cfg.xml").buildSessionFactory(); //$NON-NLS-1$
-            // break;
-            // default:
-            //                configuration.configure("hibernate_junit.cfg.xml"); //$NON-NLS-1$
-            // }
-            configuration.setProperty("current_session_context_class", "thread");
-            configuration.setProperty("cache.provider_class", "org.hibernate.cache.NoCacheProvider");
-            configuration.setProperty("show_sql", String.valueOf(usage.isShowSql()));
-            configuration.setProperty("format_sql", String.valueOf(usage.isFormatSql()));
-            try {
-                configuration.addResource("Athlete.hbm.xml");//$NON-NLS-1$
-                configuration.addResource("Health.hbm.xml"); //$NON-NLS-1$
-                configuration.addResource("Weather.hbm.xml"); //$NON-NLS-1$
-                configuration.addResource("Training.hbm.xml"); //$NON-NLS-1$
-                configuration.addResource("Trainingtype.hbm.xml"); //$NON-NLS-1$
-                configuration.addResource("Tracktrainingproperty.hbm.xml"); //$NON-NLS-1$
-                configuration.addResource("Streckenpunkte.hbm.xml"); //$NON-NLS-1$
-                configuration.addResource("Planungwoche.hbm.xml"); //$NON-NLS-1$
-                configuration.addResource("Route.hbm.xml"); //$NON-NLS-1$
-                sessionFactory = configuration.buildSessionFactory();
-            } catch (final MappingException e) {
-                e.printStackTrace();
-            }
             session = sessionFactory.openSession();
-
         }
         return session;
     }
