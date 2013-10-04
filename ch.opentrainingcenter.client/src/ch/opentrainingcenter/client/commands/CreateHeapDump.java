7a5f1d5bb09b88edbfc55c8c4043332f95adc3ab
sascha iseli <sascha.iseli@gmx.ch>
2013-10-04 15:03:20 +0200

    remove heapdump

--- ch.opentrainingcenter.client/src/ch/opentrainingcenter/client/commands/CreateHeapDump.java
+++ ch.opentrainingcenter.client/src/ch/opentrainingcenter/client/commands/CreateHeapDump.java
@@ -1,10 +1,6 @@
 package ch.opentrainingcenter.client.commands;
 
 import java.io.File;
-import java.io.IOException;
-import java.lang.management.ManagementFactory;
-
-import javax.management.MBeanServer;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.commands.AbstractHandler;
@@ -17,8 +13,6 @@ import org.joda.time.DateTime;
 
 import ch.opentrainingcenter.i18n.Messages;
 
-import com.sun.management.HotSpotDiagnosticMXBean;
-
 /**
  * Erstellt einen Heapdump snapshot in das home verzeichnis.
  * 
@@ -30,49 +24,53 @@ public class CreateHeapDump extends AbstractHandler {
 
     private static final Logger LOGGER = Logger.getLogger(CreateHeapDump.class.getName());
 
-    // This is the name of the HotSpot Diagnostic MBean
-    private static final String HOTSPOT_BEAN_NAME = "com.sun.management:type=HotSpotDiagnostic"; //$NON-NLS-1$
+    // // This is the name of the HotSpot Diagnostic MBean
+    //    private static final String HOTSPOT_BEAN_NAME = "com.sun.management:type=HotSpotDiagnostic"; //$NON-NLS-1$
+    //
+    // // field to store the hotspot diagnostic MBean
+    // private static volatile HotSpotDiagnosticMXBean hotspotMBean;
 
-    // field to store the hotspot diagnostic MBean
-    private static volatile HotSpotDiagnosticMXBean hotspotMBean;
-
-    private static final Object LOCK = new Object();
+    // private static final Object LOCK = new Object();
 
     @Override
     public Object execute(final ExecutionEvent event) throws ExecutionException {
         final String fileName = String.format("%s%s%s%s%s", System.getProperty("user.home"), File.separator, "otc_", DateTime.now().getMillis(), ".hprof"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
         LOGGER.info(String.format("create heapdump: %s", fileName)); //$NON-NLS-1$
-        try {
-            dumpHeap(fileName, true);
-        } catch (final IOException e) {
-            LOGGER.error("Heapdump konnte nicht erstellt werden", e); //$NON-NLS-1$
-        }
+        // try {
+        // dumpHeap(fileName, true);
+        // } catch (final IOException e) {
+        //            LOGGER.error("Heapdump konnte nicht erstellt werden", e); //$NON-NLS-1$
+        // }
         final Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
         MessageDialog.openInformation(shell, Messages.CreateHeapDump_0, String.format(Messages.CreateHeapDump_1, fileName));
         return null;
     }
-
-    static void dumpHeap(final String fileName, final boolean live) throws IOException {
-        // initialize hotspot diagnostic MBean
-        initHotspotMBean();
-        hotspotMBean.dumpHeap(fileName, live);
-
-    }
-
-    // initialize the hotspot diagnostic MBean field
-    private static void initHotspotMBean() throws IOException {
-        if (hotspotMBean == null) {
-            synchronized (LOCK) {
-                if (hotspotMBean == null) {
-                    hotspotMBean = getHotspotMBean();
-                }
-            }
-        }
-    }
-
-    private static HotSpotDiagnosticMXBean getHotspotMBean() throws IOException {
-        final MBeanServer server = ManagementFactory.getPlatformMBeanServer();
-        final HotSpotDiagnosticMXBean bean = ManagementFactory.newPlatformMXBeanProxy(server, HOTSPOT_BEAN_NAME, HotSpotDiagnosticMXBean.class);
-        return bean;
-    }
+    //
+    // static void dumpHeap(final String fileName, final boolean live) throws
+    // IOException {
+    // // initialize hotspot diagnostic MBean
+    // initHotspotMBean();
+    // hotspotMBean.dumpHeap(fileName, live);
+    //
+    // }
+    //
+    // // initialize the hotspot diagnostic MBean field
+    // private static void initHotspotMBean() throws IOException {
+    // if (hotspotMBean == null) {
+    // synchronized (LOCK) {
+    // if (hotspotMBean == null) {
+    // hotspotMBean = getHotspotMBean();
+    // }
+    // }
+    // }
+    // }
+    //
+    // private static HotSpotDiagnosticMXBean getHotspotMBean() throws
+    // IOException {
+    // final MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+    // final HotSpotDiagnosticMXBean bean =
+    // ManagementFactory.newPlatformMXBeanProxy(server, HOTSPOT_BEAN_NAME,
+    // HotSpotDiagnosticMXBean.class);
+    // return bean;
+    // }
 }
