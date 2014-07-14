699141e20652278fe50edfae0c998254807410eb
sascha iseli <sascha.iseli@gmx.ch>
2014-07-14 23:10:40 +0200

    sonar warnung

--- ch.opentrainingcenter.charts/src/ch/opentrainingcenter/charts/bar/internal/ChartDataWrapper.java
+++ ch.opentrainingcenter.charts/src/ch/opentrainingcenter/charts/bar/internal/ChartDataWrapper.java
@@ -71,6 +71,31 @@ public class ChartDataWrapper implements Comparable<ChartDataWrapper> {
         return date.compareTo(other.getDate());
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final ChartDataWrapper other = (ChartDataWrapper) obj;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        return true;
+    }
+
     @SuppressWarnings("nls")
     @Override
     public String toString() {
