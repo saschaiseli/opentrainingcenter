b0a211a69fff035f051d5d21f5dbbe94367c6e33
sascha iseli <sascha.iseli@gmx.ch>
2014-08-08 12:49:31 +0200

    tests

--- ch.opentrainingcenter.client.tests/src/ch/opentrainingcenter/client/model/UnitsTest.java
+++ ch.opentrainingcenter.client.tests/src/ch/opentrainingcenter/client/model/UnitsTest.java
@@ -1,9 +1,10 @@
 package ch.opentrainingcenter.client.model;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import ch.opentrainingcenter.i18n.Messages;
-import static org.junit.Assert.assertEquals;
 
 @SuppressWarnings("nls")
 public class UnitsTest {
@@ -11,6 +12,7 @@ public class UnitsTest {
     public void testEnumMitLabels() {
         assertEquals("Beats per minute", Messages.Units0, Units.BEATS_PER_MINUTE.getName());
         assertEquals("Pace", Messages.Units1, Units.PACE.getName());
+        assertEquals("Pace", Messages.Units5, Units.GESCHWINDIGKEIT.getName());
         assertEquals("Kilometer", Messages.Units2, Units.KM.getName());
         assertEquals("Stunden minuten sekunden", Messages.Units3, Units.HOUR_MINUTE_SEC.getName());
         assertEquals("Nix Einheit", Messages.Units4, Units.NONE.getName());
