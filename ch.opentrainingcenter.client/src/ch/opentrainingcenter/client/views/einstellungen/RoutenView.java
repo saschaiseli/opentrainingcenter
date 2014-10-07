6d3568ea62a54ce97501eb28fbd9cddb6854fa6b
sascha iseli <sascha.iseli@gmx.ch>
2014-10-07 20:22:18 +0200

    schuhe in eigener view

--- ch.opentrainingcenter.client/src/ch/opentrainingcenter/client/views/einstellungen/RoutenView.java
+++ ch.opentrainingcenter.client/src/ch/opentrainingcenter/client/views/einstellungen/RoutenView.java
@@ -1,32 +1,16 @@
 package ch.opentrainingcenter.client.views.einstellungen;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.viewers.DoubleClickEvent;
-import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbenchPart;
@@ -38,22 +22,15 @@ import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
 import org.eclipse.ui.part.ViewPart;
 
-import ch.opentrainingcenter.client.cache.SchuhCache;
 import ch.opentrainingcenter.client.ui.FormToolkitSupport;
 import ch.opentrainingcenter.client.ui.tableviewer.RoutenTableViewer;
-import ch.opentrainingcenter.client.ui.tableviewer.SchuhTableViewer;
 import ch.opentrainingcenter.client.ui.tableviewer.TrackTableViewer;
 import ch.opentrainingcenter.client.views.ApplicationContext;
-import ch.opentrainingcenter.client.views.dialoge.SchuhDialog;
-import ch.opentrainingcenter.core.cache.IRecordListener;
-import ch.opentrainingcenter.core.cache.TrainingCache;
-import ch.opentrainingcenter.core.data.Pair;
 import ch.opentrainingcenter.core.db.IDatabaseAccess;
 import ch.opentrainingcenter.core.service.IDatabaseService;
 import ch.opentrainingcenter.i18n.Messages;
 import ch.opentrainingcenter.transfer.IAthlete;
 import ch.opentrainingcenter.transfer.IRoute;
-import ch.opentrainingcenter.transfer.IShoe;
 import ch.opentrainingcenter.transfer.ITraining;
 
 public class RoutenView extends ViewPart implements ISelectionListener {
@@ -82,17 +59,7 @@ public class RoutenView extends ViewPart implements ISelectionListener {
 
     private RoutenTableViewer viewerRouten;
 
-    private SchuhTableViewer schuhTable;
-
     private final IDatabaseAccess databaseAccess;
-
-    private ScrolledForm formSchuhe;
-
-    private Section sectionSchuhe;
-
-    private final SchuhCache shoeCache = SchuhCache.getInstance();
-
-    private Action deleteAction;
 
     public RoutenView() {
         athlete = ApplicationContext.getApplicationContext().getAthlete();
@@ -155,176 +122,7 @@ public class RoutenView extends ViewPart implements ISelectionListener {
         sectionRouten.setClient(composite);
 
         // -------------------------------
-        formSchuhe = toolkit.createScrolledForm(main);
-        toolkit.decorateFormHeading(formSchuhe.getForm());
-        formSchuhe.setText(Messages.RoutenView_Schuhe);
-        GridLayoutFactory.swtDefaults().applyTo(formSchuhe);
-        GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(formSchuhe);
 
-        final Composite bodySchuhe = formSchuhe.getBody();
-        GridLayoutFactory.swtDefaults().applyTo(bodySchuhe);
-        GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(bodySchuhe);
-
-        sectionSchuhe = toolkit.createSection(bodySchuhe, FormToolkitSupport.SECTION_STYLE);
-        sectionSchuhe.addExpansionListener(new ExpansionAdapter() {
-            @Override
-            public void expansionStateChanged(final ExpansionEvent e) {
-                formSchuhe.reflow(true);
-            }
-        });
-        sectionSchuhe.setExpanded(true);
-
-        sectionSchuhe.setText(Messages.RoutenView_12);
-        sectionSchuhe.setDescription(Messages.RoutenView_14);
-        GridLayoutFactory.swtDefaults().applyTo(sectionRouten);
-        GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(sectionSchuhe);
-
-        final Composite compositeSchuhe = toolkit.createComposite(sectionSchuhe);
-        GridLayoutFactory.swtDefaults().applyTo(compositeSchuhe);
-        GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(compositeSchuhe);
-
-        schuhTable = new SchuhTableViewer(compositeSchuhe, SWT_TABLE_PATTERN);
-        final List<IShoe> all = shoeCache.getAll();
-        schuhTable.setAnzahlUndKilometer(getAnzahlUndKilometer(all));
-        schuhTable.createTableViewer(all);
-        final MenuManager menuManager = new MenuManager();
-        final Table table = schuhTable.getTable();
-        final Menu contextMenu = menuManager.createContextMenu(table);
-        table.setMenu(contextMenu);
-        getSite().registerContextMenu(menuManager, schuhTable);
-
-        menuManager.add(new Action(Messages.RoutenView_15, null) {
-            @Override
-            public void run() {
-                final StructuredSelection ss = (StructuredSelection) schuhTable.getSelection();
-                final IShoe shoe = (IShoe) ss.getFirstElement();
-                final SchuhDialog dialog = new SchuhDialog(main.getShell(), databaseAccess, shoe, Messages.RoutenView_16);
-                dialog.open();
-            }
-        });
-
-        deleteAction = new Action(Messages.RoutenView_17, null) {
-            @Override
-            public void run() {
-                final StructuredSelection ss = (StructuredSelection) schuhTable.getSelection();
-                final IShoe shoe = (IShoe) ss.getFirstElement();
-                databaseAccess.deleteShoe(shoe.getId());
-                shoeCache.remove(String.valueOf(shoe.getId()));
-            }
-        };
-        menuManager.add(deleteAction);
-        menuManager.add(new Action(Messages.RoutenView_18, null) {
-            @Override
-            public void run() {
-                final SchuhDialog dialog = new SchuhDialog(main.getShell(), databaseAccess, athlete, Messages.SchuhDialog_Add_Schuh);
-                dialog.open();
-            }
-        });
-        schuhTable.addSelectionChangedListener(new ISelectionChangedListener() {
-
-            @Override
-            public void selectionChanged(final SelectionChangedEvent event) {
-                final StructuredSelection ss = (StructuredSelection) event.getSelection();
-                final IShoe shoe = (IShoe) ss.getFirstElement();
-                if (shoe != null) {
-                    deleteAction.setEnabled(!hasTraining(shoe));
-                }
-            }
-        });
-
-        schuhTable.addDoubleClickListener(new IDoubleClickListener() {
-
-            @Override
-            public void doubleClick(final DoubleClickEvent event) {
-                final StructuredSelection ss = (StructuredSelection) event.getSelection();
-                final IShoe shoe = (IShoe) ss.getFirstElement();
-                final SchuhDialog dialog = new SchuhDialog(main.getShell(), databaseAccess, shoe, Messages.RoutenView_19);
-                dialog.open();
-            }
-        });
-        final Button addSchuh = new Button(compositeSchuhe, SWT.PUSH);
-        addSchuh.setText(" + "); //$NON-NLS-1$
-
-        addSchuh.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(final SelectionEvent e) {
-                final SchuhDialog dialog = new SchuhDialog(main.getShell(), databaseAccess, athlete, Messages.SchuhDialog_Add_Schuh);
-                dialog.open();
-            }
-        });
-
-        TrainingCache.getInstance().addListener(new IRecordListener<ITraining>() {
-
-            @Override
-            public void recordChanged(final Collection<ITraining> entry) {
-                update();
-            }
-
-            @Override
-            public void deleteRecord(final Collection<ITraining> entry) {
-                update();
-            }
-        });
-
-        shoeCache.addListener(new IRecordListener<IShoe>() {
-
-            @Override
-            public void recordChanged(final Collection<IShoe> entry) {
-                update();
-            }
-
-            @Override
-            public void deleteRecord(final Collection<IShoe> entry) {
-                update();
-            }
-
-        });
-        sectionSchuhe.setClient(compositeSchuhe);
-    }
-
-    private void update() {
-        Display.getDefault().asyncExec(new Runnable() {
-
-            @Override
-            public void run() {
-                final List<IShoe> shoes = databaseAccess.getShoes(athlete);
-                schuhTable.setAnzahlUndKilometer(getAnzahlUndKilometer(shoes));
-                schuhTable.setInput(shoes);
-                schuhTable.refresh();
-            }
-
-        });
-    }
-
-    private Map<IShoe, Pair<Integer, Double>> getAnzahlUndKilometer(final List<IShoe> shoes) {
-        final Map<IShoe, Pair<Integer, Double>> result = new HashMap<>();
-        for (final IShoe shoe : shoes) {
-            result.put(shoe, getAnzahlUndKilometer(shoe));
-        }
-        return result;
-    }
-
-    private Pair<Integer, Double> getAnzahlUndKilometer(final IShoe shoe) {
-        final List<ITraining> all = TrainingCache.getInstance().getAll(shoe.getAthlete());
-        int count = 0;
-        double km = 0;
-        for (final ITraining training : all) {
-            if (shoe.equals(training.getShoe())) {
-                count++;
-                km += training.getLaengeInMeter();
-            }
-        }
-        return new Pair<Integer, Double>(count, km);
-    }
-
-    private boolean hasTraining(final IShoe shoe) {
-        final List<ITraining> all = TrainingCache.getInstance().getAll(shoe.getAthlete());
-        for (final ITraining training : all) {
-            if (shoe.equals(training.getShoe())) {
-                return true;
-            }
-        }
-        return false;
     }
 
     private void createLeftComposite(final Composite composite) {
