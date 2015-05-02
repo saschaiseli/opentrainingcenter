3553430787528b94c010ac508372030af9a0813f
sascha iseli <sascha.iseli@gmx.ch>
2015-05-02 03:44:49 +0200

    das auch noch

--- ch.opentrainingcenter.route/src/ch/opentrainingcenter/route/kml/KmlTrackDumper.java
+++ ch.opentrainingcenter.route/src/ch/opentrainingcenter/route/kml/KmlTrackDumper.java
@@ -4,37 +4,28 @@ import java.util.List;
 
 import ch.opentrainingcenter.core.data.SimplePair;
 import ch.opentrainingcenter.route.IKmlDumper;
+import ch.opentrainingcenter.route.impl.TrackToKmlConverter;
 import ch.opentrainingcenter.transfer.Track;
 
 public class KmlTrackDumper implements IKmlDumper {
 
     private final String kmlPath;
+    private final TrackToKmlConverter converter;
 
     public KmlTrackDumper(final String kmlPath) {
         this.kmlPath = kmlPath;
+        converter = new TrackToKmlConverter();
     }
 
     @Override
     public void dumpTrack(final String fileName, final String label, final Track track, final List<SimplePair<String>> extendedData) {
         final KmlDumper kmlDumper = new KmlDumper(fileName, kmlPath);
-        kmlDumper.addLine(fileName, "ff0000ff", track.toKml()); //$NON-NLS-1$
-        if (!track.getPoints().isEmpty()) {
-            kmlDumper.addPlacemark("", extendedData, getRandomTrackPoint(track)); //$NON-NLS-1$
-        }
-        kmlDumper.dump();
-    }
-
-    private String getRandomTrackPoint(final Track track) {
-        final int size = track.getPoints().size() - 1;
-        final int index = 0 + (int) (Math.random() * size);
-        if (index >= size) {
-            System.err.println("??"); //$NON-NLS-1$
-        }
-        return track.getPoints().get(index).toKml();
+        kmlDumper.dump(converter.toKmlString(fileName, label, track, extendedData));
     }
 
     @Override
     public String getKmlPath() {
         return kmlPath;
     }
+
 }
